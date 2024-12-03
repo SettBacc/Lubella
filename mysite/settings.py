6402460fe010ae216487a6dd7de51309a136d0ee
Piotr Kontny <pkontny46@gmail.com>
2024-12-03 18:34:14 +0100

    dodanie include urls z mysite do urls api oraz parę zmian do loginu

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
+    'api.apps.AccountConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
