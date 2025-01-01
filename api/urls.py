269de8680c1d86be26a7af711712430090d54a7a
Piotr Kontny <pkontny46@gmail.com>
2025-01-01 20:01:15 +0100

    dodanie strony od dnia pracy oraz strony html od dodawania dnia pracy (jeszcze do stylizacji)

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 from .views import index
+from .views import add_working_day
 
 urlpatterns = [
-    #path('login/', views.login_view, name='login'),
-    #path('logout/', views.logout_view, name='logout'),
     path('', index, name='index'),  # Strona główna
+    path('add_working_day/', add_working_day, name='working_day'),  # Strona od dodawania dnia pracy
 ]
--- api/views.py
+++ api/views.py
@@ -6,11 +6,11 @@ import json
 from django.contrib.auth import logout
 from django.http import JsonResponse
 from .serializers import UserSerializer
-from .models import CustomUser, Storage, Composition, Orders
+from .models import CustomUser, Storage, Composition, Orders, WorkingDay
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .models import Product, Storage, Orders
-from .serializers import ProductSerializer, StorageSerializer, CompositionSerializer, OrdersSerializer
+from .models import Product, Storage, Orders, WorkingDay
+from .serializers import ProductSerializer, StorageSerializer, CompositionSerializer, OrdersSerializer, WorkingDaySerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -56,5 +56,23 @@ class OrdersListView(APIView):
         serializer = OrdersSerializer(orders, many=True)
         return Response(serializer.data)
 
+class WorkingDayListView(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request):
+        working_day = WorkingDay.objects.all()
+        serializer = WorkingDaySerializer(working_day, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = WorkingDaySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+
 def index(request):
     return render(request, 'index.html')
+
+def add_working_day(request):
+    return render(request, 'working_day.html')
