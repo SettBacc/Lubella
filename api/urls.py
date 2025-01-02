3cbaa57dc7a2fb6b8e6b85a5e8e53ac4b347f960
Piotr Kontny <pkontny46@gmail.com>
2025-01-02 20:16:50 +0100

    dodano jak na razie szablon tego jak będzie wyglądała strona główna z listą palet (do modyfikacji jeszcze)

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ from .views import add_working_day
 urlpatterns = [
     path('', index, name='index'),  # Strona główna
     path('add_working_day/', add_working_day, name='working_day'),  # Strona od dodawania dnia pracy
+    path('product_list/', views.product_list, name='product_list'),
 ]
